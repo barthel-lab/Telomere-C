943cbb5efeafac34c85c5c60eb15ca36d8155f43
Yi-An Chen <32481684+PersonalComputerRetailer@users.noreply.github.com>
2025-01-01 12:46:10 -0700

    Update snakemake-run.sh
    add `-n` for the dry-run as fail-safe

--- snakemake-run.sh
+++ snakemake-run.sh
@@ -10,7 +10,7 @@ CONFIGFILE="conf/config.yaml"
 
 # This command use --profile argument
 
-snakemake --use-conda --conda-prefix /tgen_labs/barthel/software/ --jobs 500 -k --profile slurm_profile -s Snakefile --verbose --cluster-config "${CLUSTRCONF}" --configfile "${CONFIGFILE}" --stats "${WORKDIR}"/logs/snakemake_stats.json all |& tee -a "${WORKDIR}"/logs/run_snakemake_"$TSTAMP".log
+snakemake -n --use-conda --conda-prefix /tgen_labs/barthel/software/ --jobs 500 -k --profile slurm_profile -s Snakefile --verbose --cluster-config "${CLUSTRCONF}" --configfile "${CONFIGFILE}" --stats "${WORKDIR}"/logs/snakemake_stats.json all |& tee -a "${WORKDIR}"/logs/run_snakemake_"$TSTAMP".log
 
 # This command DON'T use --profile argument
 #snakemake --jobs 500 -k --latency-wait 120 --max-jobs-per-second 2 --restart-times 0 --cluster-config "${CLUSTRCONF}" --configfile "${CONFIGFILE}" --jobname "{jobid}.{cluster.name}" all
